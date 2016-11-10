376875eb6c8cdc4d23fce534bf8959cb025d3eb3
micahwierenga <wierenga.micah@gmail.com>
2016-11-09 21:44:18 -0700

    Print X and O on click is working, as is the Reset Button

--- js/app.js
+++ js/app.js
@@ -14,22 +14,22 @@
 // 2. if even, print X, if odd, print O
 // 3. once a click has been made, store that click in an empty array
 // 4. if cell1 === X && cell2 === X && cell3 === X, X wins
-	if cell4 === X && cell5 === X && cell6 === X, X wins
-	if cell7 === X && cell8 === X && cell9 === X, X wins
-	if cell1 === X && cell4 === X && cell7 === X, X wins
-	if cell2 === X && cell5 === X && cell8 === X, X wins
-	if cell3 === X && cell6 === X && cell9 === X, X wins
-	if cell1 === X && cell5 === X && cell9 === X, X wins
-	if cell3 === X && cell5 === X && cell7 === X, X wins
+	// if cell4 === X && cell5 === X && cell6 === X, X wins
+	// if cell7 === X && cell8 === X && cell9 === X, X wins
+	// if cell1 === X && cell4 === X && cell7 === X, X wins
+	// if cell2 === X && cell5 === X && cell8 === X, X wins
+	// if cell3 === X && cell6 === X && cell9 === X, X wins
+	// if cell1 === X && cell5 === X && cell9 === X, X wins
+	// if cell3 === X && cell5 === X && cell7 === X, X wins
 
-	if cell1 === O && cell2 === O && cell3 === O, O wins
-	if cell4 === O && cell5 === O && cell6 === O, O wins
-	if cell7 === O && cell8 === O && cell9 === O, O wins
-	if cell1 === O && cell4 === O && cell7 === O, O wins
-	if cell2 === O && cell5 === O && cell8 === O, O wins
-	if cell3 === O && cell6 === O && cell9 === O, O wins
-	if cell1 === O && cell5 === O && cell9 === O, O wins
-	if cell3 === O && cell5 === O && cell7 === O, O wins
+	// if cell1 === O && cell2 === O && cell3 === O, O wins
+	// if cell4 === O && cell5 === O && cell6 === O, O wins
+	// if cell7 === O && cell8 === O && cell9 === O, O wins
+	// if cell1 === O && cell4 === O && cell7 === O, O wins
+	// if cell2 === O && cell5 === O && cell8 === O, O wins
+	// if cell3 === O && cell6 === O && cell9 === O, O wins
+	// if cell1 === O && cell5 === O && cell9 === O, O wins
+	// if cell3 === O && cell5 === O && cell7 === O, O wins
 
 
 
@@ -69,6 +69,8 @@ var clickCell1 = cell1.addEventListener("click", function() {
 	cell1.textContent = selectorO;
 	moveTracker.push(selectorO);
 }
+checkWinX();
+checkWinO();
 });
 
 var cell2 = document.getElementById('cell2');
@@ -80,6 +82,8 @@ var clickCell2 = cell2.addEventListener("click", function() {
 	cell2.textContent = selectorO;
 	moveTracker.push(selectorO);
 }
+checkWinX();
+checkWinO();
 });
 
 var cell3 = document.getElementById('cell3');
@@ -91,6 +95,8 @@ var clickCell3 = cell3.addEventListener("click", function() {
 	cell3.textContent = selectorO;
 	moveTracker.push(selectorO);
 }
+checkWinX();
+checkWinO();
 });
 
 var cell4 = document.getElementById('cell4');
@@ -102,6 +108,8 @@ var clickCell4 = cell4.addEventListener("click", function() {
 	cell4.textContent = selectorO;
 	moveTracker.push(selectorO);
 }
+checkWinX();
+checkWinO();
 });
 
 var cell5 = document.getElementById('cell5');
@@ -113,6 +121,8 @@ var clickCell5 = cell5.addEventListener("click", function() {
 	cell5.textContent = selectorO;
 	moveTracker.push(selectorO);
 }
+checkWinX();
+checkWinO();
 });
 
 var cell6 = document.getElementById('cell6');
@@ -124,6 +134,8 @@ var clickCell6 = cell6.addEventListener("click", function() {
 	cell6.textContent = selectorO;
 	moveTracker.push(selectorO);
 }
+checkWinX();
+checkWinO();
 });
 
 var cell7 = document.getElementById('cell7');
@@ -135,6 +147,8 @@ var clickCell7 = cell7.addEventListener("click", function() {
 	cell7.textContent = selectorO;
 	moveTracker.push(selectorO);
 }
+checkWinX();
+checkWinO();
 });
 
 var cell8 = document.getElementById('cell8');
@@ -146,6 +160,8 @@ var clickCell8 = cell8.addEventListener("click", function() {
 	cell8.textContent = selectorO;
 	moveTracker.push(selectorO);
 }
+checkWinX();
+checkWinO();
 });
 
 var cell9 = document.getElementById('cell9');
@@ -157,74 +173,90 @@ var clickCell9 = cell9.addEventListener("click", function() {
 	cell9.textContent = selectorO;
 	moveTracker.push(selectorO);
 }
+checkWinX();
+checkWinO();
 });
 
-function checkWin() {
-	if cell4 === selectorX && cell5 === selectorX && cell6 === selectorX, X wins
-	if cell7 === selectorX && cell8 === selectorX && cell9 === selectorX, X wins
-	if cell1 === selectorX && cell4 === selectorX && cell7 === selectorX, X wins
-	if cell2 === selectorX && cell5 === selectorX && cell8 === selectorX, X wins
-	if cell3 === selectorX && cell6 === selectorX && cell9 === selectorX, X wins
-	if cell1 === selectorX && cell5 === selectorX && cell9 === selectorX, X wins
-	if cell3 === selectorX && cell5 === selectorX && cell7 === selectorX, X wins
+function checkWinX() {
+	if (cell1.textContent === selectorX && cell2.textContent === selectorX && cell3.textContent === selectorX) {
+		alert("O wins");
+	}
+	else if (cell4.textContent === selectorX && cell5.textContent === selectorX && cell6.textContent === selectorX) {
+		alert("X wins");
+	}
+	else if (cell7.textContent === selectorX && cell8.textContent === selectorX && cell9.textContent === selectorX) {
+		alert("X wins");
+	}
+	else if (cell1.textContent === selectorX && cell4.textContent === selectorX && cell7.textContent === selectorX) {
+		alert("X wins");
+	}
+	else if (cell2.textContent === selectorX && cell5.textContent === selectorX && cell8.textContent === selectorX) {
+		alert("X wins");
+	}
+	else if (cell3.textContent === selectorX && cell6.textContent === selectorX && cell9.textContent === selectorX) {
+		alert("X wins");
+	}
+	else if (cell1.textContent === selectorX && cell5.textContent === selectorX && cell9.textContent === selectorX) {
+		alert("X wins");
+	}
+	else if (cell3.textContent === selectorX && cell5.textContent === selectorX && cell7.textContent === selectorX) {
+		alert("X wins");
+	} else {
+		console.log("No one has won yet");
+	}
+};
 
-	if cell1 === selectorO && cell2 === selectorO && cell3 === selectorO, O wins
-	if cell4 === selectorO && cell5 === selectorO && cell6 === selectorO, O wins
-	if cell7 === selectorO && cell8 === selectorO && cell9 === selectorO, O wins
-	if cell1 === selectorO && cell4 === selectorO && cell7 === selectorO, O wins
-	if cell2 === selectorO && cell5 === selectorO && cell8 === selectorO, O wins
-	if cell3 === selectorO && cell6 === selectorO && cell9 === selectorO, O wins
-	if cell1 === selectorO && cell5 === selectorO && cell9 === selectorO, O wins
-	if cell3 === selectorO && cell5 === selectorO && cell7 === selectorO, O wins
+function checkWinO() {
+	if (cell1.textContent === selectorO && cell2.textContent === selectorO && cell3.textContent === selectorO) {
+		alert("O wins");
+	}
+	else if (cell4.textContent === selectorO && cell5.textContent === selectorO && cell6.textContent === selectorO) {
+		alert("O wins");
+	}
+	else if (cell7.textContent === selectorO && cell8.textContent === selectorO && cell9.textContent === selectorO) {
+		alert("O wins");
+	}
+	else if (cell1.textContent === selectorO && cell4.textContent === selectorO && cell7.textContent === selectorO) {
+		alert("O wins");
+	}
+	else if (cell2.textContent === selectorO && cell5.textContent === selectorO && cell8.textContent === selectorO) {
+		alert("O wins");
+	}
+	else if (cell3.textContent === selectorO && cell6.textContent === selectorO && cell9.textContent === selectorO) {
+		alert("O wins");
+	}
+	else if (cell1.textContent === selectorO && cell5.textContent === selectorO && cell9.textContent === selectorO) {
+		alert("O wins");
+	}
+	else if (cell3.textContent === selectorO && cell5.textContent === selectorO && cell7.textContent === selectorO) {
+		alert("O wins");
+	} else {
+		console.log("No one has won yet");
+	}
+};
+
+function resetBoard() {
+	moveTracker = [];
+	console.log('reset?')
 }
 
-// var cell2 = document.getElementById('cell2');
-// var clickCell2 = cell2.addEventListener("click", function() {
-// 	makeMove();
-// });
+function clearCells() {
+	var clearCell1 = document.getElementById('cell1').innerHTML = "";
+	var clearCell2 = document.getElementById('cell2').innerHTML = "";
+	var clearCell3 = document.getElementById('cell3').innerHTML = "";
+	var clearCell4 = document.getElementById('cell4').innerHTML = "";
+	var clearCell5 = document.getElementById('cell5').innerHTML = "";
+	var clearCell6 = document.getElementById('cell6').innerHTML = "";
+	var clearCell7 = document.getElementById('cell7').innerHTML = "";
+	var clearCell8 = document.getElementById('cell8').innerHTML = "";
+	var clearCell9 = document.getElementById('cell9').innerHTML = "";
+}
 
-// var cell3 = document.getElementById('cell3');
-// var clickCell3 = cell3.addEventListener("click", function() {
-// 	makeMove();
-// });
-
-// var cell4 = document.getElementById('cell4');
-// var clickCell4 = cell4.addEventListener("click", function() {
-// 	makeMove();
-// });
-
-// var cell5 = document.getElementById('cell5');
-// var clickCell5 = cell5.addEventListener("click", function() {
-// 	makeMove();
-// });
-
-// var cell6 = document.getElementById('cell6');
-// var clickCell6 = cell6.addEventListener("click", function() {
-// 	makeMove();
-// });
-
-// var cell7 = document.getElementById('cell7');
-// var clickCell7 = cell7.addEventListener("click", function() {
-// 	makeMove();
-// });
-
-// var cell8 = document.getElementById('cell8');
-// var clickCell8 = cell8.addEventListener("click", function() {
-// 	makeMove();
-// });
-
-// var cell9 = document.getElementById('cell9');
-// var clickCell9 = cell9.addEventListener("click", function() {
-// 	makeMove();
-// });
-
-
-
-// function makeRed() {
-// 	var red = document.getElementById('cell1');
-// 	red.setAttribute("class", "printRed");
-// 	console.log('make red')
-// }
+var getResetButton = document.getElementById('resetButton');
+var resetButton = getResetButton.addEventListener("click", function() {
+	resetBoard();
+	clearCells();
+});
 
 
 
